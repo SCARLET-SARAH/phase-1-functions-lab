3b65bfd0e5631feb1441f1c29382e053d677721e
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-19 21:09:09 +0300

    create functions.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const distanceFromHqInBlocks =require(distances)
+  expect(distanceFromHqInBlocks(43)).to.equal(1);
+}
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue);
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  //returns the number of feet traveled
+}
+
+function calculatesFarePrice(start, destination) {
+  //returns the fare for the customer
+}
