2461630559b898754d6039a6a7ec726b72bd45be
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-21 12:13:45 +0300

    create function.js

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-function distanceFromHqInBlocks(someValue) {
-  const distanceFromHqInBlocks =require(distances)
-  expect(distanceFromHqInBlocks(43)).to.equal(1);
-}
-function distanceFromHqInFeet(someValue) {
-  distanceFromHqInBlocks(someValue);
-  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
-  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+function distanceFromHqInBlocks (someValue) {
 
-  // the return value of distanceFromHqInBlocks can then be used to calculate feet
-}
+  //returns the number of blocks given a value
+  if (someValue<42){
+    return(42-someValue);
+  }
+  else {
 
-function distanceTravelledInFeet(start, destination) {
-  //returns the number of feet traveled
+  return(someValue-42);
+}
 }
 
-function calculatesFarePrice(start, destination) {
-  //returns the fare for the customer
+
+
+function distanceFromHqInFeet (someValue) {
+  return distanceFromHqInBlocks(someValue)*264;
+  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  //the return value of distanceFromHqInBlocks can then be used to calculate feet
 }
+
+function distanceTravelledInFeet(a,b){
+  if (b>a){
+    return((b-a)*264);
+  }
+  else
+    return((a-b)*264);
+}
+
+
+function calculatesFarePrice(start,destination){
+  let distance = distanceTravelledInFeet(start,destination);
+  if (distance<= 400){
+    return 0;
+  }
+  if (distance>=400 && distance<=2000){
+    return (distance-400)* 0.02;
+  }
+  else if(distance>=2000&& distance<=2500){
+    return 25;
+  }
+  else if (distance > 2500){
+    return "cannot travel that far";
+  }
+}
+Footer
